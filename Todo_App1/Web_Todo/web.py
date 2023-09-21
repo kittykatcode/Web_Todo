acdc73dc83d9d7d5ef919a0cf046596df23013de
kat <katcode.py@gmail.com>
2023-09-21 12:29:20 +0530

    web todo list to be deployed

---
+++ Todo_App1/Web_Todo/web.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functionTodo
+
+st.title(' Fun ToDo App')
+st.subheader('This is my todo app')
+st.write('It will help you increase your productivity')
+
+todos = functionTodo.get_todo()
+
+def add_todo():
+        todo = st.session_state['new_todo']
+        todos.append(todo + '\n')
+        functionTodo.write_todos(todos)
+
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+          todos.pop(index)
+          functionTodo.write_todos(todos)
+          del st.session_state[todo]
+          st.experimental_rerun()
+          
+
+
+st.text_input(label= 'Enter todo here :', 
+              placeholder= 'write todo to be added to the list',
+              on_change=add_todo, key='new_todo')
